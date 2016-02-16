0c6fe3a11d8fa30d5c31980cee248bc17a29d6c5
Matej Kralik <matejkralik@hotmail.com>
2016-02-16 09:22:50 +0100

    Add local storage and modify form

--- localDB.js
+++ localDB.js
@@ -2,12 +2,40 @@
  * Created by mkralik on 2/13/16.
  */
 
-function localDB(){  //temp function
-    load("data.json");
-
+function loadDB(){  //temp function
+    if (storageAvailable('localStorage')) {
+        if(localStorage.getItem('isInLocal')){
+            loadLocal();
+        }else{ //first start
+            loadJSON("data.json");
+        }
+        showAllDB();
+    }else {
+        alert("local storage not found"); //TODO
+    }
 }
 
-function load(url) {
+function loadLocal(){
+    var data = localStorage.getItem('jsonData');
+    var tempArr = JSON.parse(data);
+    for(var i=0;i<tempArr["meals"].length;i++){
+        meals.push(new meal(tempArr["meals"][i].id,
+            tempArr["meals"][i].name,
+            tempArr["meals"][i].protein,
+            tempArr["meals"][i].carbohydrate,
+            tempArr["meals"][i].fat,
+            tempArr["meals"][i].kcal));
+    }
+    for(i=0;i<tempArr["exercises"].length;i++){
+        exercises.push(new exercise(tempArr["exercises"][i].id,
+            tempArr["exercises"][i].name,
+            tempArr["exercises"][i].kcal));
+    }
+    alert("Load DB Successful from local DB");
+    document.getElementById("loadButton").disabled=true;
+}
+
+function loadJSON(url) { //TODO refactoring
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -25,16 +53,28 @@ function load(url) {
                                         tempArr["exercises"][i].name,
                                         tempArr["exercises"][i].kcal));
             }
-            alert("Load DB Successful");
+            alert("Load DB Successful from JSON");
             document.getElementById("loadButton").disabled=true;
-
+            saveLocal();
         }
     };
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 }
 
-function saveDB(){ //TODO
+function saveLocal(){
+    var saveJSON = {
+        meals : this.meals,
+        exercises : this.exercises
+    };
+    var data = JSON.stringify(saveJSON);
+    localStorage.setItem('jsonData',data);
+    localStorage.setItem('isInLocal',true);
+    alert("DB was saved");
+
+}
+
+function saveJSON(){ //TODO download file
     var saveJSON = {
         meals : this.meals,
         exercises : this.exercises
@@ -45,40 +85,47 @@ function saveDB(){ //TODO
     window.focus();
 }
 
-function addDB(form){
-    if(form.name=="addMeal"){
-        meals.push(new meal(
-            form["id"].value,
-            form["nameMeal"].value,
-            form["protein"].value,
-            form["carbohydrate"].value,
-            form["fat"].value,
-            form["kcal"].value
-        ));
-        document.getElementById("addMealsButton").disabled=false;
-    }else if(form.name=="addExercise"){
-        exercises.push(new exercise(
-            form["id"].value,
-            form["nameExercise"].value,
-            form["kcal"].value
-        ));
-        document.getElementById("addExerciseButton").disabled=false;
-    }else{
-        alert("Unknown input");
-        form.reset();
+function deleteLocal(){
+    if (confirm("Delete local storage! Make sure that you download DB!") == true) {
+        alert("Local storage was deleted. App will be reload");
+        localStorage.clear();
+        location.reload();
+    } else {
         return;
     }
-    form.reset();
+}
+
+function addDB(mode){  //TODO way of forms ???
+    if(mode=="meal"){
+        meals.push(new meal(
+            document.forms["addMeal"][0].value,
+            document.forms["addMeal"][1].value,
+            document.forms["addMeal"][2].value,
+            document.forms["addMeal"][3].value,
+            document.forms["addMeal"][4].value,
+            document.forms["addMeal"][5].value
+    ));
+        document.getElementById("addMealsButton").disabled=false;  //TODO 2.button 2.way
+    }else if(mode=="exercise"){
+        exercises.push(new exercise(
+            document.forms[0]["id"].value,
+            document.forms[1]["nameExercise"].value,
+            document.forms[2]["kcal"].value
+        ));
+        document.getElementById("addExerciseButton").disabled=false; //TODO 2.button 2.way
+    }else{
+        alert("Unknown input");
+        return;
+    }
     refreshShowDB();
+    saveLocal();
 }
 
 function showAllDB(){
     if(meals.length==0 && exercises.length==0){
         alert("DB is empty");
-        document.getElementById("showButton").disabled=false;
         return;
     }
-
     /*
     var results = "";
     results+="<h1>MEALS</h1>";
@@ -91,7 +138,6 @@ function showAllDB(){
         results +=exercises[i] + "<br>";
     }
     document.getElementById("showDB").innerHTML = results;*/
-
     var tabMeals = document.getElementById("meals");
     var tr,node;
     for(var i=0;i<meals.length;i++){
@@ -116,7 +162,6 @@ function showAllDB(){
         node.appendChild(document.createTextNode(meals[i].kcal));
         tr.appendChild(node);
     }
-
     var tabExercises = document.getElementById("exercises");
     for(i=0;i<exercises.length;i++){
         tr = document.createElement("tr");
@@ -133,7 +178,7 @@ function showAllDB(){
     }
 }
 
-function refreshShowDB(){
+function refreshShowDB(){  // TODO right? delete and show ?
     var tabMeals = document.getElementById("headMe");
     var temp=tabMeals.nextSibling;
     for(;temp!=null;temp=tabMeals.nextSibling){
@@ -145,4 +190,18 @@ function refreshShowDB(){
         temp.remove();
     }
     showAllDB();
+    document.getElementById("refreshButton").disabled=false;
+}
+
+function storageAvailable(type) {
+    try {
+        var storage = window[type],
+            x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return false;
+    }
 }
