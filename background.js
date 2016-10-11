d7fb28da6f9df34a57f40ddb7c07b73c7738fe76
Matej Kralik <matejkralik@hotmail.com>
2016-10-11 23:14:27 +0200

    Version 1.0.1 for browsers addon

---
+++ background.js
@@ -0,0 +1,15 @@
+/*
+Open a new tab, and load "my-page.html" into it.
+*/
+function openMyPage() {
+  console.log("injecting");
+   chrome.tabs.create({
+     "url": chrome.extension.getURL("index.html")
+   });
+}
+
+/*
+Add openMyPage() as a listener to clicks on the browser action.
+*/
+chrome.browserAction.onClicked.addListener(openMyPage);
+ 
